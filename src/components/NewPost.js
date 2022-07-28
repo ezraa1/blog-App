764dce4d42edad46e5587c53d1428f15d867263d
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-07-28 09:09:58 +0300

    add an event to handle form submission

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,23 +1,28 @@
 import React, { useState } from "react";
 
 
-function NewPost(){
-    const[NewPostForm, setNewPostForm]= useState({title:"", content:"",author:""})
-    
+function NewPost(onSubmission){
+    const[newPostForm, setNewPostForm]= useState({title:"", content:"",author:""})
+
     function handleOnChange(event){
         setNewPostForm(NewPostForm=>({...NewPostForm,[event.target.name]: event.target.value}))
         }
 
+        function handleSubmit(event){
+            event.preventDefault()
+            onSubmission(newPostForm)
+            setNewPostForm({title:"",content:"", author:""})
+        }
 
 return(
 
-    <form  class="card">
+    <form onSubmit={handleSubmit} class="card">
     Title:<br></br>
-<input onChange={handleOnChange} name="title" value={NewPostForm.title} placeholder="Title" /><br></br>
+<input onChange={handleOnChange} name="title" value={newPostForm.title} placeholder="Title" /><br></br>
 Username:<br></br>
-<input onChange={handleOnChange} name="author" value={NewPostForm.author} placeholder="your username" /><br></br>
+<input onChange={handleOnChange} name="author" value={newPostForm.author} placeholder="your username" /><br></br>
 Content:<br></br>
-<textarea  onChange={handleOnChange} name="content" value={NewPostForm.content} placeholder="Type the news..." rows={20} /><br></br>
+<textarea  onChange={handleOnChange} name="content" value={newPostForm.content} placeholder="Type the news..." rows={20} /><br></br>
 <input type="submit" value="Submit" />
 </form>
 )
