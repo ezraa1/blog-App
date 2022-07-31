60cde668989ce6761870a23a9cecc7bb5d50ca67
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-07-31 23:33:17 +0300

    change details on about component

--- src/components/About.js
+++ src/components/About.js
@@ -2,15 +2,17 @@ import React from "react";
 
 
 function About(){
-    function handleAbout(e){
-
-    }
+  const imgUrl= 'https://media.istockphoto.com/photos/sports-balls-on-the-field-with-yard-line-soccer-ball-american-and-picture-id942206100?b=1&k=20&m=942206100&s=170667a&w=0&h=S6SIMeZy5KWm-SEmOAlW-TGTUqCEvtK9WrupPA7qLKg='
     return(
-        <div>
-            <p>We enable tranparency in providing our Sports news.</p>
-            <div>
-            <p>Check up daily to get more detailed Sports news</p>
-            </div>
+        <div className="image">
+         <img src={imgUrl} alt="sportImage" />
+            <p></p>
+        
+            <p><h1>Check up daily to get more detailed Sports news</h1>
+            <h2>We enable tranparency in providing our Sports news.</h2>
+            </p>
+            
+
         </div>
         
     )
