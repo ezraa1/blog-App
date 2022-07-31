df7ef8b31f9f05f42e849c53e271b25d101c024d
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-08-01 00:11:38 +0300

    handle effect on form submit

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,10 +2,25 @@ import React, { useState } from "react";
 import NewPost from "./NewPost";
 
 function Login(){
+  const [posts, setPosts] =useState([])
   const [show, setShow] = useState(false);
 
   function handleClick(){
+    
     setShow(show=>!show)
+    function handleNewPost(newFormData){
+      const serverOptions={
+          method:"POST",
+          headers:{
+          "Content-Type":"application/json"
+          },
+          body:JSON.stringify(newFormData
+          )
+          }
+          fetch('http://localhost:3000/posts', serverOptions)
+          .then(r=>r.json())
+          .then(addedPost=>setPosts(posts=>[...posts, addedPost]))
+      }
   }
     return(
         <div className="login">
